8f5cd42fd95088206b0911a5ac247e0bf0ba1dc3
gaarafive <dattranjd@gmail.com>
2020-08-14 23:56:41 +0700

    🍧 init

--- characters.js
+++ characters.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Character = require('./Character');
+const Character = require('./character');
 
 router.get('/', async (req, res) => {
     try {
         const characters = await Character.find();
         res.json(characters);
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        res.status(500).json({
+            message: err.message
+        });
     }
 });
 
@@ -20,7 +22,9 @@ router.post('/', async (req, res) => {
         const newCharacter = await character.save();
         res.status(201).json(newCharacter);
     } catch (err) {
-        res.status(400).json({ message: err.message });
+        res.status(400).json({
+            message: err.message
+        });
     }
 });
 
--- server.js
+++ server.js
@@ -2,17 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const cors = require('cors');
+const path = require('path');
+const serveStatic = require('serve-static');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+});
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
+db.on('error', (error) => console.error('error', error));
 db.once('open', () => console.log('Connected to database!'));
 
 server.use(express.json());
 server.use(cors());
+server.use(serveStatic(__dirname + '/client/dist'));
 
 const router = require('./characters');
 server.use('/characters', router);
 
-server.listen(3000, () => console.log("Server started!"));
+const port = process.env.PORT || 3100
+server.listen(port, () => console.log("Server started!"));
